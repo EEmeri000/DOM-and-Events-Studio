20d1ff51ef1e35231af2154be61253fda9491c73
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-07-26 17:20:51 -0400

    Started studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    let takeOff = document.getElementById("takeoff");
+    let flightStatus = document.getElementById("flightStatus");
+
+    takeOff.addEventListener('click', event => {
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+              flightStatus.innerHTML = 'Shuttle in flight.';
+    });
+});
